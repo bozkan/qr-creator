c04ba9022697e9a0e784cf4d61105e3fcde81954
Bugra Ozkan <bugraozkan@live.com>
2024-06-07 18:35:46 +0100

    Init qr creator

---
+++ qr.py
@@ -0,0 +1,24 @@
+import qrcode
+from IPython.display import display
+
+# Define the LinkedIn URL
+linkedin_url = "https://www.linkedin.com/in/torayeff/"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(linkedin_url)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill='black', back_color='white')
+
+# Display the QR code
+display(img)
+
+# Save the QR code to a file
+img.save("qrAgajan.png")
